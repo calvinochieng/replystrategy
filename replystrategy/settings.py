d4b29a7c05bfa3c21745d3bd22da2317c38a5e5e
clv <calvinfpage@gmail.com>
2025-01-10 00:36:03 +0300

    Added author details

--- replystrategy/settings.py
+++ replystrategy/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='fallback-secret-key')
 
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True or config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*', '127.0.0.1/']
 
