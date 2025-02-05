00aba1f9901491ea14de982371d3f63dfca79714
clv <calvinfpage@gmail.com>
2025-02-05 22:00:04 +0300

    Finished the blogging system _

--- replystrategy/settings.py
+++ replystrategy/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 
-DEBUG = True or  config('DEBUG',  cast=bool)
+DEBUG = config('DEBUG',  cast=bool)
 
 PRODUCTION_MODE = config('PRODUCTION_MODE',cast=bool)
 
